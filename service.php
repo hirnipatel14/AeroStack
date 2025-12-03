c49194b6df3a8e80796878dd510e1008a3b308f0
jay.m@hbsoftweb.in <jay.m@hbsoftweb.in>
2025-12-03 11:37:30 +0530

    Update service.php

--- service.php
+++ service.php
@@ -788,7 +788,7 @@
                   select the section and use the icons above to make your
                   changes.
                 </p>
-                <a href="cloud-hosting.html" class="theme-btn mt-4"
+                <a href="#" class="theme-btn mt-4"
                   >Best hosting <i class="fa-regular fa-chevrons-right"></i
                 ></a>
               </div>
@@ -809,7 +809,7 @@
                   using the right icon of each block. Then, duplicate one of the
                   columns to create a new one as a copy.
                 </p>
-                <a href="reseller-hosting.html" class="theme-btn mt-4"
+                <a href="#" class="theme-btn mt-4"
                   >Best hosting <i class="fa-regular fa-chevrons-right"></i
                 ></a>
               </div>
@@ -832,7 +832,7 @@
                   conveys your message. Click on the image to adjust its rounded
                   corner style.
                 </p>
-                <a href="cloud-hosting.html" class="theme-btn mt-4"
+                <a href="#" class="theme-btn mt-4"
                   >Best hosting <i class="fa-regular fa-chevrons-right"></i
                 ></a>
               </div>
